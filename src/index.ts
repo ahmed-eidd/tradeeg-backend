c87638066a5c273e0c14c2e0a76ccdfbab680567
Ahmed Eid <ahmedeid.webdev@gmail.com>
2026-09-14 13:00:06 +0300

    change cors

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,12 @@ import authRouter from './auth/router';
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL || 'http://localhost:5173',
+    credentials: true,
+  }),
+);
 
 app.use(
   session({
